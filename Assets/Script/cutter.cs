525168de18968f4add703fbe8500996a9e1336d4
JeongEon8 <113885866+JeongEon8@users.noreply.github.com>
2023-01-12 13:58:35 +0900

    update0112_Slicegame
    Slicegame을 만들어 보았다. 하지만 기획과 아무것도 맞게 되지 않았다.

---
+++ Assets/Script/cutter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
+
+public class cutter : MonoBehaviour
+{
+    Vector3 randomAngle;
+    public GameObject Knife;
+
+    void OnTriggerEnter(Collider col)
+    {
+        int count = 0;
+        if (col.gameObject.tag == "Slice")
+        {   
+            col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            col.gameObject.GetComponent<Rigidbody>().AddTorque(-Vector3.up * 12000f, ForceMode.Impulse);
+            randomAngle = new Vector3(Random.Range(-0.8f, -2f), Random.Range(0.2f, 0.3f), Random.Range(-2f, 2f));
+
+            col.gameObject.GetComponent<Rigidbody>().AddForce(randomAngle * Random.Range(1, 2), ForceMode.Impulse);
+
+        }
+    }
+
+/*    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Slice" && Knife.GetComponent<Knife>().isCutting)
+        {
+            col.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            col.gameObject.GetComponent<Rigidbody>().AddTorque(-Vector3.up * 12000f, ForceMode.Impulse);
+            randomAngle = new Vector3(Random.Range(-0.8f, -2f), Random.Range(0.2f, 0.3f), Random.Range(-2f, 2f));
+
+            col.gameObject.GetComponent<Rigidbody>().AddForce(randomAngle * Random.Range(650, 1500), ForceMode.Impulse);
+            Knife.GetComponent<Knife>().SetCuttingState(true);
+
+            Destroy(col.gameObject, 4f);
+        }
+    }*/
+}
